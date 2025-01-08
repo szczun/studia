00a64823d387a657b9043b8d1a79723a34a6f997
szczun <szcisek@student.agh.edu.pl>
2025-01-08 19:08:29 +0100

    ciasto lab2

--- ciasto/Projekty/Cieplo-wlasciwe/macro.C
+++ ciasto/Projekty/Cieplo-wlasciwe/macro.C
@@ -7,7 +7,8 @@
 #include <TCanvas.h>
 #include <TLatex.h>
 #include <TF1.h>
-
+#include <TStyle.h>
+#include <TLegend.h>
 
 void macro(){
     std::string filename = "Au.dat";
@@ -30,11 +31,12 @@ void macro(){
 
 
     TGraph* graph = new TGraph();
+    TLegend* legend = new TLegend(0.2, 0.6, 0.4, 0.8);
 
     graph->SetMarkerStyle(kFullCircle);
-    graph->SetTitle("Wykres ciepła właściwego w funkcji temperatury");
+    // graph->SetTitle("Wykres ciepła właściwego w funkcji temperatury");
     graph->GetXaxis()->SetTitle("Temperatura [K]");
-    graph->GetYaxis()->SetTitle("Ciepło właściwe [J mol^{-1} K^{-1}]");
+    graph->GetYaxis()->SetTitle("Cieplo wlasciwe [J mol^{-1} K^{-1}]");
 
     while(!file.eof()){
         file >> temperature >> s_heat_cap;
@@ -47,7 +49,7 @@ void macro(){
     file.close();
 
     TF1* fn = new TF1("fn", "[0]*x + 3*(6.02e23)*(1.38e-23)*0.8*TMath::Pi()^4 * (x/[1])^3", temperatures[0], temperatures[temperatures.size()-1]);
-    fn->SetParameter(0, 0.005);
+    fn->SetParameter(0, 0.006);
     fn->SetParameter(1, 170);
 
     TCanvas* canvas = new TCanvas("c1", "Canvas for plot", 800, 600);
@@ -56,4 +58,10 @@ void macro(){
     canvas->SetGridy();
     graph->SetStats(0);
     graph->Fit("fn", "R");
+    legend->AddEntry(graph, "Dane");
+    legend->AddEntry(fn, "Dopasowana krzywa");
+    legend->SetBorderSize(0);
+    legend->SetTextSize(0.03);
+    legend->Draw();
+    canvas->SaveAs("wykres.pdf");
 }
